5d323002d11c9a197b9a5030e3fb2c1f5ac922ba
Max Fry <m.sukach@innopolis.ru>
2018-08-29 16:17:48 +0300

    Fix

--- week2/task1.c
+++ week2/task1.c
@@ -6,7 +6,7 @@ int main()
   int a=MAX_INT;
   float b=FLT_MAX;
   double d=DBL_MAX;
-  printf("The integer is %d\n", a);
+  printf("The integer is %i\n", a);
   printf("The float is %d\n", a);
   printf("The float is %f\n", a);
 }
