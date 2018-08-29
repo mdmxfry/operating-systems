db16245dc608df001e95eb5de955730f700af5cc
Max Fry <m.sukach@innopolis.ru>
2018-08-29 16:10:10 +0300

    First Try

---
+++ week2/task1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+  int a=MAX_INT;
+  float b=FLT_MAX;
+  double d=DBL_MAX;
+  printf("The integer is %d\n", a);
+  printf("The float is %f\n", a);
+  printf("The float is %d\n", a);
+}
+
