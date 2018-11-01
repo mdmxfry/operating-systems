40a393e201d2b31cc28a8b2e140de81283a891c3
Max Fry <m.sukach@innopolis.ru>
2018-11-01 22:05:05 +0300

    FIXED: EX2

--- week11/ex2.c
+++ week11/ex2.c
@@ -3,22 +3,18 @@
 #include <string.h>
 
 int main() {
-    int C_SIZE = 1024;
+    int C_SIZE = 64;
     char buffer[C_SIZE];
     char source[] = "Hello";
 
-    FILE *fp = fopen("ex2.txt", "w+");
-    setvbuf(fp, buffer, _IOLBF, C_SIZE);
+    setvbuf(stdout, buffer, _IOLBF, C_SIZE);
 
     for (int i = 0; i < strlen(source); i++) {
         sleep(1);
-        fputs("H", fp);
+        printf("H");
     }
 
-    fputs("\n", fp);
-
-    rewind(fp);
-    fclose(fp);
+    printf("\n");
 
     return 0;
 }
